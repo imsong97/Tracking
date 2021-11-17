98f8e6553380176604b0ce63fc5af26d1b608205
sy <sy@sweettracker.co.kr>
2021-11-17 11:57:21 +0900

    viewmodel 수정

---
+++ app/src/main/java/com/yunho/tracking/domain/ViewModel.kt
@@ -0,0 +1,22 @@
+package com.yunho.tracking.domain
+
+import com.yunho.tracking.domain.model.TrackingData
+import java.text.SimpleDateFormat
+
+class ViewModel(private val trackingData: TrackingData){
+
+    fun getStatusText(): String = getState(trackingData.trackingDetail!!)
+
+    fun getTimeText(): String = "도착예정시간 : ${trackingData.parcelDeliverTime}"
+
+    fun setProgress(): Int = trackingData.parcelLevel!!
+
+    fun getRegDateText(): String = "등록일 : ${getDate()}"
+
+    private fun getState(data: List<TrackingData.Detail>): String = data[data.size -1].status.toString()
+
+    private fun getDate(): String{
+        val form = SimpleDateFormat("yyyy-MM-dd")
+        return form.format(System.currentTimeMillis())
+    }
+}
